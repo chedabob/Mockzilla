69e1c5cb169904f89ffc99ee6118c1b934797bb2
chedabob <matt@mattmalone.app>
2024-05-16 21:26:25 +0100

    temp: stuff

--- lib/mockzilla/src/androidMain/kotlin/com/apadmi/mockzilla/lib/internal/utils/DiscoveryService.kt
+++ lib/mockzilla/src/androidMain/kotlin/com/apadmi/mockzilla/lib/internal/utils/DiscoveryService.kt
@@ -1,7 +1,47 @@
 package com.apadmi.mockzilla.lib.internal.utils
 
+import android.content.Context
+import android.net.nsd.NsdManager
+import android.net.nsd.NsdServiceInfo
+
 internal actual class DiscoveryService actual constructor(x: String) {
     actual fun makeDiscoverable() {
-        throw NotImplementedError()
+        val serviceInfo = NsdServiceInfo().apply {
+            // The name is subject to change based on conflicts
+            // with other services advertised on the same network.
+            serviceName = "test1234"
+            serviceType = "_mockzilla._tcp"
+            port = 8080
+        }
+
+
+        val nsdManager = (getSystemService(Context.NSD_SERVICE) as NsdManager).apply {
+            registerService(serviceInfo, NsdManager.PROTOCOL_DNS_SD, registrationListener)
+        }
+
     }
+
+    private val registrationListener = object : NsdManager.RegistrationListener {
+
+        override fun onServiceRegistered(serviceInfo: NsdServiceInfo) {
+            // Save the service name. Android may have changed it in order to
+            // resolve a conflict, so update the name you initially requested
+            // with the name Android actually used.
+            mServiceName = serviceInfo.serviceName
+        }
+
+        override fun onRegistrationFailed(serviceInfo: NsdServiceInfo, errorCode: Int) {
+            // Registration failed! Put debugging code here to determine why.
+        }
+
+        override fun onServiceUnregistered(serviceInfo: NsdServiceInfo) {
+            // Service has been unregistered. This only happens when you call
+            // NsdManager.unregisterService() and pass in this listener.
+        }
+
+        override fun onUnregistrationFailed(serviceInfo: NsdServiceInfo, errorCode: Int) {
+            // Unregistration failed. Put debugging code here to determine why.
+        }
+    }
+
 }
